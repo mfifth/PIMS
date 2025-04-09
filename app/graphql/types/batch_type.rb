6a0b30e552519df77e33a839a8f5b2dfd807de86
Matt <mfifth@gmail.com>
2025-04-08 17:50:14 -0700

    Update batch column datatypes for GraphQL

--- app/graphql/types/batch_type.rb
+++ app/graphql/types/batch_type.rb
@@ -4,7 +4,7 @@ module Types
     field :batch_number, String, null: true
     field :category, String, null: true
     field :notification_days_before_expiration, Integer, null: true
-    field :expiration_date, Date, null: true
-    field :manufactured_date, Date, null: true
+    field :expiration_date, GraphQL::Types::ISO8601Date, null: true
+    field :manufactured_date, GraphQL::Types::ISO8601Date, null: true
   end
 end
