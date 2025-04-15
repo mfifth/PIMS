f148ffbc049195fe6fe741f324917167585ac3ce
Matt <Matt@Peters-MacBook.local>
2025-04-15 13:40:20 -0700

    Update prod config

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,6 @@ class Notification < ApplicationRecord
   validates :message, presence: true
   validates :notification_type, presence: true
   belongs_to :account
+
+  scope :unread, -> { where(read: false) }
 end
