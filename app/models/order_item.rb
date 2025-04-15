dfcc5a8dc634b2deb70283958b1bc9514c6ac992
Matt <Matt@Peters-MacBook.local>
2025-04-14 19:12:17 -0700

    Remove models

--- app/models/order_item.rb
+++
@@ -1,9 +0,0 @@
-class OrderItem < ApplicationRecord
-  belongs_to :order
-  belongs_to :product
-  belongs_to :batch, optional: true
-  belongs_to :location
-
-  # Add fields like quantity, price, etc.
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
-end
