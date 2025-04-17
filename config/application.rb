0ea00a009f04de3624226be6aba4284af38f2f91
Matt <Matt@Peters-MacBook.local>
2025-04-17 16:00:10 -0700

    update config

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Pims
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_paths << "#{Rails.root}/app/middleware"
 
     config.importmap.enabled = true
 
