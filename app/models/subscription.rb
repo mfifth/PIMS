4e514eb11f00145b1fe5419015777ca48440b969
Matt <Matt@Peters-MacBook.local>
2025-04-16 18:42:11 -0700

    Hide notification settings behind subscription plan

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,22 +3,19 @@ class Subscription < ApplicationRecord
 
   PRODUCT_PLAN_LIMITS = {
     'free' => 25,
-    'basic' => 50,
     'pro' => 100,
     'enterprise' => Float::INFINITY
   }
 
   USER_PLAN_LIMITS = {
     'free' => 1,
-    'basic' => 2,
-    'pro' => 3,
+    'pro' => 2,
     'enterprise' => Float::INFINITY
   }
 
   LOCATION_PLAN_LIMITS = {
     'free' => 1,
-    'basic' => 2,
-    'pro' => 4,
+    'pro' => 2,
     'enterprise' => Float::INFINITY
   }
 end
