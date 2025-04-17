753c3d61962ef2300609a3045d90530888bb9f10
Matt <Matt@Peters-MacBook.local>
2025-04-17 16:08:16 -0700

    remove middleware

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Pims
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.autoload_paths << "#{Rails.root}/app/middleware"
 
     config.importmap.enabled = true
 
@@ -32,8 +31,6 @@ module Pims
       env.register_mime_type('application/javascript', extensions: ['.js'])
       env.register_preprocessor('application/javascript', Sprockets::DirectiveProcessor)
     end
-
-    config.middleware.insert_before Rack::Sendfile, RequestBlocker
 
     # Configuration for the application, engines, and railties goes here.
     #
