1eb75b6b91e76e1e230a4c8e7251ff7399fbecbc
Matt <Matt@Peters-MacBook.local>
2025-04-17 18:04:00 -0700

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.authenticate_by(params.permit(:email_address, :password))
-    msg = user&.confirmed_at ? "Try another email address or password." : "Please confirm your email before logging in."
+    msg = user.confirmed_at.present? ? "Try another email address or password." : "Please confirm your email before logging in."
 
-    if user && user.confirmed_at
+    if user && user.confirmed_at.present?
       start_new_session_for user
       redirect_to after_authentication_url
     else
