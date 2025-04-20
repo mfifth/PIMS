e9d6aa4e44eb247eefbd66478c05eb7ed55de8b7
Matt <Matt@Peters-MacBook.local>
2025-04-20 16:50:57 -0700

    fix active product filter color

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,12 +1,18 @@
 module LocationsHelper
   def filter_link_class(filter_type, value)
     active = case filter_type
-             when :perishable then params[:perishable] == value.to_s
-             when :low_stock then params[:low_stock] == value.to_s
-             when :expiring then params[:expiring] == value.to_s
-             else params[:perishable].blank? && params[:low_stock].blank? && params[:expiring].blank?
+             when :perishable then params[:perishable].present? && to_bool(params[:perishable]) == value
+             when :low_stock then params[:low_stock].present? && to_bool(params[:low_stock]) == value
+             when :expiring then params[:expiring].present? && to_bool(params[:expiring]) == value
+             else
+               params[:perishable].blank? && params[:low_stock].blank? && params[:expiring].blank?
              end
-  
-    active ? "bg-blue-500 text-white px-3 py-1 rounded" : "bg-gray-200 text-gray-700 px-3 py-1 rounded hover:bg-gray-300"
+
+    base_classes = "px-3 py-1 rounded transition-colors duration-200"
+    active ? "#{base_classes} bg-blue-500 text-white" : "#{base_classes} bg-gray-200 text-gray-700 hover:bg-gray-300"
+  end
+
+  def to_bool(param)
+    ActiveRecord::Type::Boolean.new.cast(param)
   end
 end
