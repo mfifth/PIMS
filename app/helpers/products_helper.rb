b8a28a113e98ced249fcadab2713c21080ac4a7d
Matt <Matt@Peters-MacBook.local>
2025-04-25 18:37:36 -0700

    updates

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,11 +5,9 @@ module ProductsHelper
   end
 
   def show_batch_info(product)
-    return unless product.batch
-
-    mfg = product.batch.manufactured_date || I18n.t('products.na')
-    exp = product.batch.expiration_date || I18n.t('products.na')
-    "#{I18n.t('products.mfg')}: #{mfg} - #{I18n.t('products.exp')}: #{exp} |"
+    mfg = product&.batch&.manufactured_date || I18n.t('products.na')
+    exp = product&.batch&.expiration_date || I18n.t('products.na')
+    "#{I18n.t('products.mfg')}: #{mfg} - #{I18n.t('products.exp')}: #{exp}"
   end
 
   def convert_units(quantity, unit_type)
