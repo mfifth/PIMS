a3fddd5cd6819b9a04d0a329568b5c6d408d8b52
Matt <Matt@Peters-MacBook.local>
2025-04-28 13:03:34 -0700

    Update clover sync service

--- app/services/clover_sync_service.rb
+++ app/services/clover_sync_service.rb
@@ -5,28 +5,36 @@ class CloverSyncService
 
   def sync_all
     fetch_all_items.each do |item|
-      next unless item["stockCounts"]
+      if meal_item?(item)
+        sync_recipe(item)
+      else
+        product = sync_product(item)
 
-      product = sync_product(item)
-
-      item["stockCounts"].each do |stock|
-        next unless stock["location"]
-
-        sync_inventory_item(product, stock)
+        if item["stockCounts"]
+          item["stockCounts"].each do |stock|
+            next unless stock["location"]
+  
+            sync_inventory_item(product, stock)
+          end
+        end
       end
     end
-  end
+  end  
+
+  private
+
+  attr_reader :account
 
   def fetch_all_items
     all_items = []
     cursor = nil
 
     loop do
-      url = "https://api.clover.com/v3/merchants/#{@account.clover_merchant_id}/items?expand=stockCounts&limit=100"
+      url = url = "https://api.clover.com/v3/merchants/#{account.clover_merchant_id}/items?expand=stockCounts,categories&limit=100"
       url += "&cursor=#{cursor}" if cursor
 
       response = Faraday.get(url) do |req|
-        req.headers["Authorization"] = "Bearer #{@account.clover_access_token}"
+        req.headers["Authorization"] = "Bearer #{account.clover_access_token}"
       end
 
       data = JSON.parse(response.body)
@@ -37,10 +45,6 @@ class CloverSyncService
 
     all_items
   end
-
-  private
-
-  attr_reader :account
 
   def sync_product(item)
     product = account.products.find_or_initialize_by(sku: item["id"])
@@ -61,4 +65,26 @@ class CloverSyncService
     inventory_item.quantity = stock["quantity"].to_i
     inventory_item.save!
   end
+
+  def sync_recipe(item)
+    recipe = account.recipes.find_or_initialize_by(uid: item["id"])
+    recipe.name = item["name"]
+    recipe.price = item["price"].to_i
+    recipe.save!
+  end
+
+  def meal_item?(item)
+    name = item["name"].to_s
+    description = item["description"].to_s
+    category_name = ""
+  
+    if item["categories"] && item["categories"].any?
+      category_name = item["categories"].first["name"].to_s
+    end
+  
+    MealClassifier.new(
+      OpenStruct.new(item_data: OpenStruct.new(name: name, description: description)),
+      category_name
+    ).meal?
+  end  
 end
