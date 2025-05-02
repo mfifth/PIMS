38c36d0871468514c4e59eb6179ee4c7940c4a0c
Matt <Matt@Peters-MacBook.local>
2025-05-02 16:44:11 -0700

    update service to use inventory_item unit_type

--- app/services/recipe_order_processor_service.rb
+++ app/services/recipe_order_processor_service.rb
@@ -6,25 +6,25 @@ class RecipeOrderProcessorService
   def process_recipe(recipe, quantity_sold)
     recipe.recipe_items.each do |recipe_item|
       product = recipe_item.product
-      next unless product&.unit_type && recipe_item.quantity.to_f > 0 && quantity_sold.to_f > 0
+      next unless product && recipe_item.quantity.to_f > 0 && quantity_sold.to_f > 0
 
-      converted_quantity = if recipe_item.unit == product.unit_type
-        recipe_item.quantity.to_f
-      else
-        recipe_item.quantity.to_f * conversion_rate(recipe_item.unit, product.unit_type)
-      end
+      inventory_item = @location.inventory_items.find_by(product: product)
+      next unless inventory_item
+
+      converted_quantity = if recipe_item.unit == inventory_item.unit_type
+                            recipe_item.quantity.to_f
+                          else
+                            recipe_item.quantity.to_f * conversion_rate(recipe_item.unit, inventory_item.unit_type)
+                          end
 
       quantity_to_deduct = converted_quantity * quantity_sold.to_f
-      deduct_inventory(product, -quantity_to_deduct)
+      deduct_inventory(inventory_item, -quantity_to_deduct)
     end
   end
 
   private
 
-  def deduct_inventory(product, quantity_to_deduct)
-    inventory_item = @location.inventory_items.find_by(product: product)
-    return unless inventory_item
-  
+  def deduct_inventory(inventory_item, quantity_to_deduct)
     new_quantity = inventory_item.quantity - quantity_to_deduct
     inventory_item.quantity = [new_quantity, 0].max
     inventory_item.save!
@@ -35,10 +35,10 @@ class RecipeOrderProcessorService
       'grams' =>   { 'grams' => 1, 'ounces' => 1.0 / 28.3495, 'pounds' => 1.0 / 453.592 },
       'ounces' =>  { 'grams' => 28.3495, 'ounces' => 1, 'pounds' => 1.0 / 16 },
       'pounds' =>  { 'grams' => 453.592, 'ounces' => 16, 'pounds' => 1 },
-      'liters' =>  { 'liters' => 1, 'gallons' => 1.0 / 3.78541, 'fluid_oz' => 33.814, 'ml' => 1000 },
-      'gallons' => { 'liters' => 3.78541, 'gallons' => 1, 'fluid_oz' => 128, 'ml' => 3785.41 },
-      'fluid_oz' => { 'liters' => 0.0295735, 'gallons' => 1.0/128, 'fluid_oz' => 1, 'ml' => 29.5735 },
-      'ml' =>      { 'liters' => 0.001, 'gallons' => 0.000264172, 'fluid_oz' => 0.033814, 'ml' => 1 },
+      'liters' =>  { 'liters' => 1, 'gallons' => 1.0 / 3.78541, 'fluid_oz' => 33.814, 'milliliters' => 1000 },
+      'gallons' => { 'liters' => 3.78541, 'gallons' => 1, 'fluid_oz' => 128, 'milliliters' => 3785.41 },
+      'fluid_oz' => { 'liters' => 0.0295735, 'gallons' => 1.0 / 128, 'fluid_oz' => 1, 'milliliters' => 29.5735 },
+      'milliliters' => { 'liters' => 0.001, 'gallons' => 0.000264172, 'fluid_oz' => 0.033814, 'milliliters' => 1 },
       'units' =>   { 'units' => 1 }
     }.dig(from_unit, to_unit) || 1
   end
