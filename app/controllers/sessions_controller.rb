248bc06fb8fdadea26ec984c042dcda411180bcf
Matt <Matt@Peters-MacBook.local>
2025-05-03 17:11:26 -0700

    minor updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.authenticate_by(params.permit(:email_address, :password))
-    msg = user&.confirmed_at.present? ? t('notifications.try_another_login') : t('notifications.confirm_email')
-
-    if user && user.confirmed_at.present?
-      start_new_session_for user
+    
+    if user&.confirmed_at.present?
+      reset_session
+      start_new_session_for(user)
       redirect_to after_authentication_url
     else
-      redirect_to new_session_path, alert: msg
+      redirect_to new_session_path, 
+        alert: user ? t('notifications.confirm_email') : t('notifications.try_another_login')
     end
   end
 
