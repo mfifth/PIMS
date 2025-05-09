0555aa9bc19acf5137160890fa8889ce76889f7c
Matt <Matt@Peters-MacBook.local>
2025-05-08 20:05:20 -0700

    optimize job for expiration notifications

--- app/jobs/expiration_notification_job.rb
+++ app/jobs/expiration_notification_job.rb
@@ -4,26 +4,35 @@ class ExpirationNotificationJob < ApplicationJob
   def perform
     today = Date.current
 
-    Batch.where.not(expiration_date: nil).find_each do |batch|
-      next unless batch.notification_days_before_expiration.present?
-
+    Batch.where.not(expiration_date: nil, notification_days_before_expiration: nil)
+    .find_each do |batch|
       notify_date = batch.expiration_date - batch.notification_days_before_expiration.days
-      text = t('notifications.batch_expiry', batch_number: batch.batch_number, 
-      product_count: batch.products.count, expiration_date: batch.expiration_date)
 
-      if today == notify_date
-        batch.products.each do |product|
-          product.accounts.distinct.each do |account|
-            Notification.create(
-              message: text,
-              notification_type: "Alert",
-              account_id: account.id
-            )
-            account.users.each do |user|
-              NotificationMailer.upcoming_expiration_date(user, batch).deliver_later if user.email_notification
-              NotificationService.send_sms(user.phone, text) if user.text_notification && user.phone
-            end
-          end
+      next unless today == notify_date
+
+      text = t('notifications.batch_expiry', 
+               batch_number: batch.batch_number, 
+               product_count: batch.products.count, 
+               expiration_date: batch.expiration_date)
+
+      accounts = batch.products.includes(:accounts).map(&:accounts).flatten.uniq
+      users = accounts.flat_map(&:users).uniq
+
+      accounts.each do |account|
+        Notification.create(
+          message: text,
+          notification_type: "Alert",
+          account_id: account.id
+        )
+      end
+
+      users.each do |user|
+        if user.email_notification
+          NotificationMailer.upcoming_expiration_date(user, batch).deliver_later
+        end
+
+        if user.text_notification && user.phone
+          NotificationService.send_sms(user.phone, text)
         end
       end
     end
