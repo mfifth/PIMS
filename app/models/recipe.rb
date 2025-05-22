62353f5ae117eaa2ac83e191f128fe118a3530b5
Matt <Matt@Peters-MacBook.local>
2025-05-21 18:29:10 -0700

    update logic for estimation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,7 +10,16 @@ class Recipe < ApplicationRecord
   validate :recipe_limit_not_exceeded, on: :create
 
   def estimated_cost(location)
-    recipe_items.sum { |item| item.cost_per_recipe(location) }
+    recipe_items.sum do |item|
+      inventory_item = item.product.inventory_items.where(location: location).first
+      next item.product.price.to_f * item.quantity unless inventory_item
+      
+      conversion_rate = RecipeItem::CONVERSION_RATES.dig(item.unit, inventory_item.unit_type)
+      next item.product.price.to_f * item.quantity unless conversion_rate
+      
+      adjusted_quantity = item.quantity * conversion_rate
+      adjusted_quantity * inventory_item.price
+    end
   end
 
   private
