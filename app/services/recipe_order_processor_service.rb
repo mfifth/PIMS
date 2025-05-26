8a52ab57034c318ddcee922c40d6dc977d4b19c7
Matt <Matt@Peters-MacBook.local>
2025-05-25 17:40:58 -0700

    refactor service

--- app/services/recipe_order_processor_service.rb
+++ app/services/recipe_order_processor_service.rb
@@ -11,12 +11,7 @@ class RecipeOrderProcessorService
       inventory_item = @location.inventory_items.find_by(product: product)
       next unless inventory_item
 
-      converted_quantity = if recipe_item.unit == inventory_item.unit_type
-                             recipe_item.quantity.to_f
-                           else
-                             recipe_item.quantity.to_f * conversion_rate(recipe_item.unit, inventory_item.unit_type)
-                           end
-
+      converted_quantity = recipe_item.convert_quantity(recipe_item.quantity.to_f, inventory_item.unit_type)
       quantity_to_deduct = converted_quantity * quantity_sold.to_f
       deduct_inventory(inventory_item, quantity_to_deduct)
     end
@@ -52,7 +47,7 @@ class RecipeOrderProcessorService
   
       if inventory_item.quantity < 1 && current_index + 1 < chain.length
         next_unit = chain[current_index + 1]
-        inventory_item.quantity *= conversion_rate(inventory_item.unit_type, next_unit)
+        inventory_item.quantity *= RecipeItem::CONVERSION_RATES.dig(inventory_item.unit_type, next_unit) || 1
         inventory_item.unit_type = next_unit
   
         Rails.logger.info "Inventory #{inventory_item.product.name} switched to #{next_unit} (quantity: #{inventory_item.quantity})"
@@ -60,56 +55,5 @@ class RecipeOrderProcessorService
         break
       end
     end
-  end  
-
-  def conversion_rate(from_unit, to_unit)
-    {
-      'grams' => {
-        'grams' => 1,
-        'ounces' => 1.0 / 28.3495,
-        'pounds' => 1.0 / 453.592
-      },
-      'ounces' => {
-        'grams' => 28.3495,
-        'ounces' => 1,
-        'pounds' => 1.0 / 16
-      },
-      'pounds' => {
-        'grams' => 453.592,
-        'ounces' => 16,
-        'pounds' => 1
-      },
-      'kilograms' => {
-        'grams' => 1000,
-        'kilograms' => 1
-      },
-      'liters' => {
-        'liters' => 1,
-        'gallons' => 1.0 / 3.78541,
-        'fluid_oz' => 33.814,
-        'milliliters' => 1000
-      },
-      'gallons' => {
-        'liters' => 3.78541,
-        'gallons' => 1,
-        'fluid_oz' => 128,
-        'milliliters' => 3785.41
-      },
-      'fluid_oz' => {
-        'liters' => 0.0295735,
-        'gallons' => 1.0 / 128,
-        'fluid_oz' => 1,
-        'milliliters' => 29.5735
-      },
-      'milliliters' => {
-        'liters' => 0.001,
-        'gallons' => 0.000264172,
-        'fluid_oz' => 0.033814,
-        'milliliters' => 1
-      },
-      'units' => {
-        'units' => 1
-      }
-    }.dig(from_unit, to_unit) || 1
-  end  
+  end
 end
