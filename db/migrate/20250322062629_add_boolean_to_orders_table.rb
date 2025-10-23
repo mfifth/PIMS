dc994da831cd12c4480397f9af70d23cbf23b916
Matt Quinto <mfifth@gmail.com>
2025-10-22 20:51:49 -0700

    Fix order migrations for Postgres deployment

--- db/migrate/20250322062629_add_boolean_to_orders_table.rb
+++ db/migrate/20250322062629_add_boolean_to_orders_table.rb
@@ -1,5 +1,15 @@
 class AddBooleanToOrdersTable < ActiveRecord::Migration[8.0]
-  def change
+  def up
+    return unless table_exists?(:orders)
+    return if column_exists?(:orders, :replenish_on_arrival)
+
     add_column :orders, :replenish_on_arrival, :boolean, default: false
+  end
+
+  def down
+    return unless table_exists?(:orders)
+    return unless column_exists?(:orders, :replenish_on_arrival)
+
+    remove_column :orders, :replenish_on_arrival
   end
 end
